87420c047879dfa1cd61e57dcea5ae96116aafc3
Sriram Jayashankar <sriramjayashankar@gmail.com>
2023-11-14 21:11:29 +0530

    password hashed and salted using bcrypt

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongouri = 'mongodb://127.0.0.1:27017/'
+const mongouri = 'mongodb://127.0.0.1:27017/inotebook'
 
 const connectToMongo = () => {
     mongoose.connect(mongouri).then(console.log("connected to mongoose")).catch((e) => { console.log(e) })
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,72 @@
 const express = require('express')
 const router = express.Router()
-const User=require("../models/User.js")
-
+const User = require("../models/User.js")
+const bcrypt=require("bcryptjs")
 const { body, validationResult } = require('express-validator');
 
-router.post('/',async (req,res)=>{
-    console.log(req.body)
-    const user=new User(req.body)
-    await user.save()
-    res.send(req.body)
-})
+//check with a post req in api/auth/create user No login required this is for user signup
+router.post('/createuser',
+    [body("name", "enter a valid name").isLength({ min: 5 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password", "enter a valid password with atleast 5 characters").isLength({ min: 5 })]
+    ,
+    async (req, res) => {
+        //if there are errors return errors
+        try{
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() })
+        }
+        const k=await User.findOne({email:req.body.email})
+        console.log(k)
+        if(k)
+        {
+            return res.status(400).json({ errors: "sorry A user with the given email id already exists" })
+        }
+        const salt=await bcrypt.genSalt(10)
+        const hashpwd=await bcrypt.hash(req.body.password,salt)
+        console.log(hashpwd)
+         const user=await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashpwd
 
+        })
+        res.send(user)
+    }
+    catch(error){
+        console.log("theres an error given below"+error)
+        res.status(500).send("theres an error given below")
+    }
+    })
+
+
+
+
+
+//check with a post req in api/auth/login user No login required
+router.post('/login',
+[body("email", "enter a valid email").isEmail(),
+body("password", "cannotbeblank").exists(),
+]
+,async (req, res) => {
+    //if there are errors return errors
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).json({ errors: result.array() })
+    }
+    const {name,email,password}=req.body
+    try{
+        let user=User.findOne({email})
+        if(user)
+        {
+            return res.status(400).json({ errors: "Pls try to login correct" })
+        }
+        const passwordcompare=bcry
+
+    }catch(error)
+    {
+
+    }
+})
 module.exports = router
